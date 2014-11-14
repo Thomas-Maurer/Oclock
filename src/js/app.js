6e23b83549fd589d0804ca4f1a0f4ecbbb7825eb
Thomas Maurer <toto-ange@hotmail.fr>
2014-11-14 16:40:00 +0100

    Add back button + route

--- src/js/app.js
+++ src/js/app.js
@@ -11,19 +11,19 @@ oclockApp.config(['$routeProvider', '$locationProvider', function ($routeProvide
           controller: 'AcceuilController',
           controllerAs: 'AcceuilController'
         })
+        .when('/home', {
+          templateUrl: 'partials/home.html',
+          controller: 'AcceuilController',
+          controllerAs: 'AcceuilController'
+        })
         .otherwise({
         redirect: '/'
     });
 
       $locationProvider.html5Mode(true);
 }]);
-oclockApp.controller('OclockController',['$scope', function ($scope) {
+oclockApp.controller('OclockController',['$scope', '$rootScope', function ($scope, $rootScope) {
   'use strict';
-  $scope.clock = {
-    adresse: "clock1",
-    heure: "",
-    minutes: ""
-  };
   var stage = new Kinetic.Stage({
     container: 'game',
     width: 800,
@@ -32,7 +32,7 @@ oclockApp.controller('OclockController',['$scope', function ($scope) {
   var layer = new Kinetic.Layer();
   stage.add(layer);
   var img = new Image();
-  img.src = 'css/asset/' + $scope.clock.adresse + '.png';
+  img.src = 'css/asset/' + $rootScope.clock.adresse + '.png';
   img.onload = function () {
       var imgclock = new Kinetic.Image({
         width: 250,
@@ -45,8 +45,13 @@ oclockApp.controller('OclockController',['$scope', function ($scope) {
     layer.draw();
   };
 }]);
-oclockApp.controller('AcceuilController',['$scope', function ($scope) {
-  $scope.launchGame = function () {
-    console.log('test click');
+oclockApp.controller('AcceuilController',['$scope', '$rootScope', function ($scope, $rootScope) {
+  $rootScope.clock = {
+    adresse: "clock1",
+    heure: "",
+    minutes: ""
   };
+}]);
+oclockApp.controller('OptionController',['$scope', function ($scope) {
+
 }]);
